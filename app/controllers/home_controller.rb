9378e50b342972f70d8b5fcb4af81c10ed6f4ab3
Nelson Minor Haraguchi jr <nelsonmhjr@gmail.com>
2010-03-19 18:07:18 -0300

    adicionando redirecionamento para /v3

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
-  before_filter :authenticate_usuario!
 
   def index
+    redirect_to "/v3/index.php"
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -33,12 +33,6 @@ ActionController::Routing::Routes.draw do |map|
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   map.root :controller => "home"
+  map.connect "/v3", :controller => "home"
 
-  # See how all your routes lay out with "rake routes"
-
-  # Install the default routes as the lowest priority.
-  # Note: These default routes make all actions in every controller accessible via GET requests. You should
-  # consider removing or commenting them out if you're using named routes and resources.
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
 end
