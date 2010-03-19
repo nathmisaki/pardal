b3083f8b23e3d0e858b08f70694984a1945ae880
Nelson Minor Haraguchi jr <nelsonmhjr@gmail.com>
2010-03-18 21:06:46 -0300

    adicionando steps recomendadas no wiki do devise

--- features/step_definitions/application_steps.rb
+++ features/step_definitions/application_steps.rb
@@ -1,9 +1,27 @@
+Dado /^que eu não estou autenticado$/ do
+  visit(destroy_usuario_session_path)
+end
+
+Dado /^que eu tenha um usuário "([^\"]*)" com senha "([^\"]*)" e login "([^\"]*)"$/ do |email, password, login|
+  User.new(:email => email,
+           :login => login,
+           :password => password,
+           :password_confirmation => password).save!
+end
+
 Dado /^que eu seja um usuário logado$/ do
-  pending
+  email = 'testing@man.net'
+  login = 'Testing man'
+  password = 'secretpass'
+
+  Dado %{que eu tenha um usuário "#{email}" com senha "#{password}" e login "#{login}"}
+  E %{eu vou para login}
+  E %{eu preencho "user_email" com "#{email}"}
+  E %{eu preencho "user_password" com "#{password}"}
+  E %{eu pressiono em "usuario_submit"}
 end
 
 Dado /^que eu tenha um registro de ([^\s]+) com os campos:$/ do |modelo, table|
-  # table is a Cucumber::Ast::Table
-  pending # express the regexp above with the code you wish you had
+  
 end
 
