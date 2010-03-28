4596f8c8aa0737e44fcc1c59d463f6169266df9e
Nelson Minor Haraguchi jr <nelsonmhjr@gmail.com>
2010-03-28 20:59:56 -0300

    Adicionando Acl9

--- config/environment.rb
+++ config/environment.rb
@@ -24,6 +24,7 @@ Rails::Initializer.run do |config|
   config.gem "formtastic"
   config.gem "inherited_resources"
   config.gem "responders"
+  config.gem "acl9", :lib => "acl9"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
