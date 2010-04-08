a4f9b759808c8bfb51e00edac02f7ef581324654
Nelson Minor Haraguchi jr <nelsonmhjr@gmail.com>
2010-04-08 09:16:00 -0300

    Mais testes unitarios do user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,6 +13,40 @@ describe User do
         :identity_emission_date => @student.identity_emission_date.strftime("%d/%m/%Y")}
     end
 
+    context "with link_student_*" do
+      before(:each) do
+        @user.link_student=@link_student
+      end
+      context "registration" do
+        it { @user.link_student_registration.should == @link_student[:registration] }
+        it {
+          @user.link_student_registration = "H1010101"
+          @user.link_student[:registration].should == "H1010101"
+        }
+      end
+      context "identity" do
+        it { @user.link_student_identity.should == @link_student[:identity] }
+        it {
+          @user.link_student_identity = 'ASDF'
+          @user.link_student[:identity].should == 'ASDF'
+        }
+      end
+      context "identity_emission_date" do
+        it { @user.link_student_identity_emission_date.should == @link_student[:identity_emission_date] }
+        it {
+          @user.link_student_identity_emission_date = '27/06/1986'
+          @user.link_student[:identity_emission_date].should == '27/06/1986'
+        }
+      end
+      context "mothers_name_initials" do
+        it { @user.link_student_mothers_name_initials.should == @link_student[:mothers_name_initials] }
+        it {
+          @user.link_student_mothers_name_initials = 'ADF'
+          @user.link_student[:mothers_name_initials].should == 'ADF'
+        }
+      end
+    end
+
     it 'should have role owner and student when linking student' do
       @user.link_student = @link_student
 
