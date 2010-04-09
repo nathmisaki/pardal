b291e7a338451dd1655b7edf5f4a77ecd4bb6a61
Nelson Minor Haraguchi jr <nelsonmhjr@gmail.com>
2010-04-08 23:20:58 -0300

    Adicionando AttachmentController

---
+++ app/controllers/attachments_controller.rb
@@ -0,0 +1,20 @@
+class AttachmentsController < ApplicationController
+  def show
+    @attachment = Attachment.find(params[:id])
+    send_data @attachment.data,
+      :filename => @attachment.file_name,
+      :type => @attachment.content_type
+  end
+
+  def create
+    return if params[:attachment].blank?
+
+    @attachment = Attachment.new
+    @attachment.uploaded_file = params[:attachment]
+
+    if @attachment.save
+      redirect_to :back
+    end
+  end
+
+end
