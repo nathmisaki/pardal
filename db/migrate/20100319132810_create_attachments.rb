faa65376a645fa56997a3479fc5b1c9b5d09f05d
Nelson Minor Haraguchi jr <nelsonmhjr@gmail.com>
2010-04-08 23:08:05 -0300

    Mudando os campos do Attachment

--- db/migrate/20100319132810_create_attachments.rb
+++ db/migrate/20100319132810_create_attachments.rb
@@ -3,16 +3,16 @@ class CreateAttachments < ActiveRecord::Migration
     create_table :attachments do |t|
       t.string :attachable_type, :limit => 30
       t.integer :attachable_id
-      t.string :attach_file_name, :limit => 100
-      t.string :attach_content_type, :limit => 50
-      t.integer :attach_file_size
-      t.datetime :attach_updated_at
+      t.string :file_name, :limit => 100
+      t.string :content_type, :limit => 50
+      t.integer :file_size
+      t.binary :data
 
       t.timestamps
     end
   end
 
   def self.down
-    drop_table :attachs
+    drop_table :attachments
   end
 end
