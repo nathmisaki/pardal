0be23cce1c26c5fa19b80915a96de8263cabe5a4
Nelson Minor Haraguchi jr <nelsonmhjr@gmail.com>
2010-04-10 11:01:40 -0300

    Adicionando curriculum no student

--- spec/models/curriculum_spec.rb
+++ spec/models/curriculum_spec.rb
@@ -1,16 +1,4 @@
 require 'spec_helper'
 
 describe Curriculum do
-  before(:each) do
-    @valid_attributes = {
-      :school_id => 1,
-      :period_id => 1,
-      :curriculum_type => 1,
-      :implementation_semester => 1
-    }
-  end
-
-  it "should create a new instance given valid attributes" do
-    Curriculum.create!(@valid_attributes)
-  end
 end
