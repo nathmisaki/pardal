72b80c6a7834eb43907cc0be2db689dde4c25b7c
Nelson Minor Haraguchi jr <nelsonmhjr@gmail.com>
2010-04-11 23:38:03 -0300

    Blueprint

--- spec/blueprints.rb
+++ spec/blueprints.rb
@@ -84,7 +84,6 @@ DisciplineType.blueprint do
 end
 
 CourseSchool.blueprint do
-  course { Course.make }
   period { Period.make }
   code { (1..300).to_a.shuffle.shift }
   symbol { (65..90).to_a.map(&:chr).shuffle.shift }
