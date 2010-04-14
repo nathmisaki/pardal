12709a31b941f3d4c0afa19dee40b1ad74189051
Nelson Minor Haraguchi jr <nelsonmhjr@gmail.com>
2010-04-13 22:22:16 -0300

    tentando fazer passar o cucumber

--- config/environment.rb
+++ config/environment.rb
@@ -19,11 +19,12 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem "warden"
   config.gem "devise", :version => '1.0.4'
   config.gem "formtastic"
   config.gem "inherited_resources", :version => '1.0.6'
   config.gem "acl9", :lib => "acl9"
+  config.gem "haml"
+  config.gem "compass"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
