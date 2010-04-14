b8e2b52b7f3baa22378645af70fba1ea5555f4ec
Nelson Minor Haraguchi jr <nelsonmhjr@gmail.com>
2010-04-13 23:12:49 -0300

    Copiando esquema de configuracao de email do redmine

--- config/initializers/mail_config_loader.rb
+++ config/initializers/mail_config_loader.rb
@@ -1,5 +1,17 @@
-# Load mail configuration if not in test environment
-if RAILS_ENV != 'test'
-  email_settings = YAML::load(File.open("#{RAILS_ROOT}/config/email.yml"))
-  ActionMailer::Base.smtp_settings = email_settings[RAILS_ENV] unless email_settings[RAILS_ENV].nil?
+# Loads action_mailer settings from email.yml
+# # and turns deliveries on if configuration file is found
+#
+filename = File.join(File.dirname(__FILE__), '..', 'email.yml')
+if File.file?(filename)
+  mailconfig = YAML::load_file(filename)
+
+  if mailconfig.is_a?(Hash) && mailconfig.has_key?(Rails.env)
+    # Enable deliveries
+    ActionMailer::Base.perform_deliveries = true
+
+    mailconfig[Rails.env].each do |k, v|
+      v.symbolize_keys! if v.respond_to?(:symbolize_keys!)
+      ActionMailer::Base.send("#{ k}=", v)
+    end
+  end
 end
