143d357b9c1acc98fbc40a8012eb52e7b11f21b8
Bruno Coimbra <bbcoimbra@gmail.com>
2010-04-24 00:52:20 -0300

    include primary_key and relationship into model

--- lib/legacy_discipline.rb
+++ lib/legacy_discipline.rb
@@ -1,5 +1,7 @@
 class LegacyDiscipline < Academnew
-
   set_table_name "disciplinas"
+  set_primary_key "CodigoDaDisciplina"
+
+  belongs_to :legacy_department, :foreign_key => 'CodigoDoDepartamento'
 
 end
