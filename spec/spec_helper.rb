d6c3ebb35cf1310b41d743cfe782205c99b3c70a
Nelson Minor Haraguchi jr <nelsonmhjr@gmail.com>
2010-04-24 19:38:03 -0300

    instalando spork

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,75 +1,53 @@
-# This file is copied to ~/spec when you run 'ruby script/generate rspec'
-# from the project root directory.
 ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path(File.join(File.dirname(__FILE__),'..','config','environment'))
-require 'spec/autorun'
-require 'spec/rails'
+require 'rubygems'
+require 'spork'
 
-require File.expand_path(File.dirname(__FILE__) + "/blueprints")
+Spork.prefork do
+  require File.expand_path(File.join(File.dirname(__FILE__),'..','config','environment'))
+  require 'spec/autorun'
+  require 'spec/rails'
+  require 'webrat/integrations/rspec-rails'
 
-# Uncomment the next line to use webrat's matchers
-require 'webrat/integrations/rspec-rails'
+  # Requires supporting files with custom matchers and macros, etc,
+  # in ./support/ and its subdirectories.
+  Dir[File.expand_path(File.join(File.dirname(__FILE__),'support','**','*.rb'))].each {|f| require f}
 
-# Requires supporting files with custom matchers and macros, etc,
-# in ./support/ and its subdirectories.
-Dir[File.expand_path(File.join(File.dirname(__FILE__),'support','**','*.rb'))].each {|f| require f}
+  # Requires support classes to custom rake tasks
+  Dir[File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib','*.rb'))].each{|f| require f}
 
-# Requires support classes to custom rake tasks
-Dir[File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib','*.rb'))].each{|f| require f}
-
-Spec::Runner.configure do |config|
-  # If you're not using ActiveRecord you should remove these
-  # lines, delete config/database.yml and disable :active_record
-  # in your config/boot.rb
-  config.use_transactional_fixtures = true
-  config.use_instantiated_fixtures  = false
-  config.fixture_path = RAILS_ROOT + '/spec/fixtures/'
-  config.before(:all)     { Sham.reset(:before_all)   }
-  config.before(:each)    { Sham.reset(:before_each)  }
-  config.before(:suite)   do
-    if defined?(ActiveRecord::Base)
-      begin
-        require 'database_cleaner'
-        DatabaseCleaner.strategy = :truncation
-        DatabaseCleaner.clean
-      rescue LoadError => ignore_if_database_cleaner_not_present
+  Spec::Runner.configure do |config|
+    # If you're not using ActiveRecord you should remove these
+    # lines, delete config/database.yml and disable :active_record
+    # in your config/boot.rb
+    config.use_transactional_fixtures = true
+    config.use_instantiated_fixtures  = false
+    config.fixture_path = RAILS_ROOT + '/spec/fixtures/'
+    config.before(:all)     { Sham.reset(:before_all)   }
+    config.before(:each)    { Sham.reset(:before_each)  }
+    config.before(:suite)   do
+      if defined?(ActiveRecord::Base)
+        begin
+          require 'database_cleaner'
+          DatabaseCleaner.strategy = :truncation
+          DatabaseCleaner.clean
+        rescue LoadError => ignore_if_database_cleaner_not_present
+        end
       end
     end
+
   end
 
-  # == Fixtures
-  #
-  # You can declare fixtures for each example_group like this:
-  #   describe "...." do
-  #     fixtures :table_a, :table_b
-  #
-  # Alternatively, if you prefer to declare them only once, you can
-  # do so right here. Just uncomment the next line and replace the fixture
-  # names with your fixtures.
-  #
-  # config.global_fixtures = :table_a, :table_b
-  #
-  # If you declare global fixtures, be aware that they will be declared
-  # for all of your examples, even those that don't use them.
-  #
-  # You can also declare which fixtures to use (for example fixtures for test/fixtures):
-  #
-  # config.fixture_path = RAILS_ROOT + '/spec/fixtures/'
-  #
-  # == Mock Framework
-  #
-  # RSpec uses its own mocking framework by default. If you prefer to
-  # use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
-  #
-  # == Notes
-  #
-  # For more information take a look at Spec::Runner::Configuration and Spec::Runner
+  class ActionController::TestCase
+    include Devise::TestHelpers
+  end
 end
 
-class ActionController::TestCase
-  include Devise::TestHelpers
+Spork.each_run do
+  require File.expand_path(File.dirname(__FILE__) + "/blueprints")
 end
+
+# - These instructions should self-destruct in 10 seconds.  If they don't,
+#   feel free to delete them.
+#
+
+
