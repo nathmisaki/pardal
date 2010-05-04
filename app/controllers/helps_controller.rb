e4499eb1d65e87cf697373fa3e003188476bec95
Nelson Minor Haraguchi jr <nelsonmhjr@gmail.com>
2010-05-04 20:05:17 -0300

    Alterando help para suportar wildcards

--- app/controllers/helps_controller.rb
+++ app/controllers/helps_controller.rb
@@ -1,6 +1,6 @@
 class HelpsController < InheritedResources::Base
   def show
-    @help = Help.find_by_page(params[:id])
+    @help = Help.find_page(params[:id])
     super do |format|
       format.html { request.xhr? ? render(:show, :layout => false) : render(:show)}
     end
--- app/models/help.rb
+++ app/models/help.rb
@@ -1,2 +1,5 @@
 class Help < ActiveRecord::Base
+  def self.find_page(page_name)
+    self.find_by_page(page_name.gsub(/-[\d]+-/, '-#-'))
+  end
 end
