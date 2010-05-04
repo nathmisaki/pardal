eeaff7adebd9e1244b9cd4877937957fe938bcf8
Bruno Coimbra <bbcoimbra@gmail.com>
2010-05-04 14:34:16 -0300

    pre-implementation for School23

--- lib/proposals/school23.rb
+++ lib/proposals/school23.rb
@@ -1,5 +1,15 @@
 module Proposals
   class School23 < Proposal
+    def calculateEnrollments
+      disciplines = student.curriculum.disciplines
+      disciplines = disciplines_without_concluded(disciplines)
+      disciplines = student.disciplines_with_pre_requirements_concluded(disciplines)
+
+      courses = courses_from_curriculum(disciplines)
+
+      enrollments = enrollments_from_courses(courses)
+
+    end
 
   end
 end
