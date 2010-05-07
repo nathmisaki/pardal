06a93bedfcd4cbea4c58ec2df8cb49b3c55dd142
Nelson Minor Haraguchi jr <nelsonmhjr@gmail.com>
2010-05-06 22:10:35 -0300

    Retirando teste de rota

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,4 @@ ActionController::Routing::Routes.draw do |map|
 
   map.root :controller => "home"
   map.connect "/v3", :controller => "home"
-  map.test_named "/test_named/:p1/:p2/:p3", :controller => "home", :action => 'index', :p1 => 1, :p2 => 2, :p3 => 3
 end
