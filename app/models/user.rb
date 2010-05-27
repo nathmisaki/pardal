64ad536d5654fe3a74218d95d741ce926226a241
Nelson Minor Haraguchi jr <nelsonmhjr@gmail.com>
2010-05-26 23:00:34 -0300

    Refatoração de long lines e comentários Adicionando nova associação students

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 # encoding: utf-8
 class User < ActiveRecord::Base
-  devise :authenticatable, :confirmable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+  devise :authenticatable, :confirmable, :registerable,
+    :recoverable, :rememberable, :trackable, :validatable
   acts_as_authorization_subject
 
   has_many :attachments, :as => :attachable
   has_one :profile
+  has_many :students
 
   attr_accessor :link_student
 
@@ -34,18 +36,24 @@ class User < ActiveRecord::Base
     when is_email
       self.find_by_email(condition[:email])
     when is_student_registration
-      student = Student.find_by_registration(Student.registration_with_initial_letter(condition[:email]))
+      student = Student.find_by_registration(
+        Student.registration_with_initial_letter(condition[:email]))
       student.user
     else
       self.find_by_email(condition[:email])
     end
   end
 
+  def has_students
+
+  end
+
   def objects_with_role(*args)
     role_name = args.shift
     options = args.extract_options!
     conditions = { :name => role_name.to_s }
-    conditions.merge(:authorizable_type => options[:type].to_s.classify) unless options[:type].nil?
+    conditions.merge(:authorizable_type =>
+                     options[:type].to_s.classify) unless options[:type].nil?
     ret = role_objects.all(:conditions => conditions)
     ret.map! { |ro| ro.authorizable } unless options[:role]
     ret
@@ -58,6 +66,7 @@ class User < ActiveRecord::Base
   def link_student_registration
     link_student[:registration] if @link_student.is_a?(Hash)
   end
+
   def link_student_registration=(value)
     link_student[:registration] = value
   end
@@ -65,6 +74,7 @@ class User < ActiveRecord::Base
   def link_student_identity
     link_student[:identity] if @link_student.is_a?(Hash)
   end
+
   def link_student_identity=(value)
     link_student[:identity] = value
   end
@@ -72,6 +82,7 @@ class User < ActiveRecord::Base
   def link_student_identity_emission_date
     link_student[:identity_emission_date] if @link_student.is_a?(Hash)
   end
+
   def link_student_identity_emission_date=(value)
     link_student[:identity_emission_date] = value
   end
@@ -79,11 +90,14 @@ class User < ActiveRecord::Base
   def link_student_mothers_name_initials
     link_student[:mothers_name_initials] if @link_student.is_a?(Hash)
   end
+
   def link_student_mothers_name_initials=(value)
     link_student[:mothers_name_initials] = value
   end
 
   def attach_student!(student)
+    student.user_id = self.id
+    student.save
     self.has_role!(:owner, student)
     self.has_role!(:student, student)
   end
@@ -96,33 +110,56 @@ class User < ActiveRecord::Base
     end
   end
 
+  # Método chamado para validar entrada de novo Aluno no usuário
+  #
+  # Aqui são verificadas as variáveis que entram pelo formulário de
+  # ligação de alunos: matrícula, identidade, data de emissão de
+  # identidade, e iniciais do nome da mãe.
+  #
   def validate_link_student
+
+    # se a matrícula está vazia, é inválida!
     if @link_student[:registration].blank?
       errors.add(:link_student_registration, :invalid)
-    else
-      student = Student.find_by_registration(@link_student[:registration])
-      if student.nil?
-        errors.add(:link_student_registration, :not_find)
-      else
-        unless student.accepted_roles.all(:conditions => { :name => 'owner' }).empty?
-          errors.add(:link_student_registration, :taken)
-        else
-          errors.add(:link_student_identity, :not_match) unless \
-            student.identity.gsub(/\D/,'') == @link_student[:identity].gsub(/\D/,'')
-
-          begin
-            errors.add(:link_student_identity_emission_date, :not_match) unless \
-              student.identity_emission_date == Date.new(*@link_student[:identity_emission_date].split('/').reverse.map(&:to_i))
-          rescue ArgumentError
-            errors.add(:link_student_identity_emission_date, :invalid)
-          end
-
-          errors.add(:link_student_mothers_name_initials, :not_match) unless \
-            student.valid_mothers_name_initials?(@link_student[:mothers_name_initials])
-        end
-      end
+      return nil
     end
 
+    student = Student.find_by_registration(@link_student[:registration])
+
+    # se não encontrei o aluno
+    if student.nil?
+      errors.add(:link_student_registration, :not_find)
+      return nil
+    end
+
+    # o aluno já tem dono
+    unless student.user_id.nil?
+      errors.add(:link_student_registration, :taken)
+    end
+
+    # identidade diferente
+    errors.add(:link_student_identity, :not_match) unless \
+      student.identity.gsub(/\D/,'') == @link_student[:identity].gsub(/\D/,'')
+
+    begin
+      date = @link_student[:identity_emission_date].split('/')
+      date = date.reverse.map(&:to_i)
+
+      # data de emissão diferente
+      errors.add(:link_student_identity_emission_date, :not_match) unless \
+        student.identity_emission_date == Date.new(*date)
+    rescue ArgumentError
+      # data de emissão inválida
+      errors.add(:link_student_identity_emission_date, :invalid)
+    end
+
+    # iniciais do nome da mãe diferentes dos válidos
+    errors.add(:link_student_mothers_name_initials, :not_match) unless \
+      student.valid_mothers_name_initials?(
+        @link_student[:mothers_name_initials])
+
+    # se tudo está OK (sem erros) setar @link_student com o modelo student
+    # encontrado para ser processado pelo método new_link_student
     if errors.empty?
       @link_student = student
     end
