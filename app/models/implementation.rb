fe415ac568bc5efaca9b53423ca20dcbc06eede8
Nelson Haraguchi <nelsonmhjr@gmail.com>
2010-06-01 11:13:56 -0300

    Using EnumerateIt in Implementation to DisciplineType

--- app/models/implementation.rb
+++ app/models/implementation.rb
@@ -2,5 +2,11 @@ class Implementation < ActiveRecord::Base
   has_many :pre_requirements
   belongs_to :curriculum
   belongs_to :discipline
-  belongs_to :discipline_type
+  include EnumerateIt
+  has_enumeration_for :discipline_type_id, :with => DisciplineType
+
+  def discipline_type
+    discipline_type_id_humanize
+  end
 end
+
