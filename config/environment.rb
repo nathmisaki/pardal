4b41cacf06a2ebd3871106ab82868beeb378e65e
Nelson Haraguchi <nelsonmhjr@gmail.com>
2010-06-01 10:51:35 -0300

    Installing EnumerateIt

--- config/environment.rb
+++ config/environment.rb
@@ -26,6 +26,7 @@ Rails::Initializer.run do |config|
   config.gem "acl9", :lib => "acl9"
   config.gem "haml"
   config.gem "compass"
+  config.gem "enumerate_it"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
