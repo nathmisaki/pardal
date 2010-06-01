09ca5950aadad5f723b9a386925792dc7ff189c9
Nelson Haraguchi <nelsonmhjr@gmail.com>
2010-06-01 11:02:34 -0300

    NamedScopes do CourseSemester

--- app/models/course_semester.rb
+++ app/models/course_semester.rb
@@ -1,4 +1,14 @@
 class CourseSemester < ActiveRecord::Base
   belongs_to :course
   has_many :course_schedules
+
+  named_scope :only_current_semester, :conditions => { :semester => Time.now.year_semester }
+
+  named_scope :courses_in, lambda { |*courses|
+    courses.flatten!
+    courses.map!(&:id) if courses.first.is_a?(Course)
+    { :conditions => { :course_id => courses } }
+  }
+
+  named_scope :including_course_and_discipline, :include => [:course => :discipline]
 end
