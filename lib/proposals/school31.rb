92a6ba40c9cb8bcbf829ffedb2da1aca655091c3
Nelson Haraguchi <nelsonmhjr@gmail.com>
2010-06-01 11:32:54 -0300

    Commenting nminus3 (tests not implemented)

--- lib/proposals/school31.rb
+++ lib/proposals/school31.rb
@@ -3,7 +3,7 @@ module Proposals
     def calculateEnrollments
       disciplines = student.curriculum.disciplines
       disciplines = disciplines_without_concluded(disciplines)
-      disciplines = student.n_minus_3(disciplines)
+      #disciplines = student.n_minus_3(disciplines)
 
       courses = courses_from_curriculum(disciplines)
 
