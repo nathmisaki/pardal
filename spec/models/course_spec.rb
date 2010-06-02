109866e2683443f49f45896b566b68578aafa354
Nelson Haraguchi <nelsonmhjr@gmail.com>
2010-06-01 21:18:08 -0300

    Adding tests for current_course_semester

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,4 +1,13 @@
 require 'spec_helper'
 
 describe Course do
+  context "with #current_course_semester" do
+    before(:each) do
+      @course = Course.make
+      @course_semester = CourseSemester.make(:course => @course, :semester => Time.now.year_semester)
+      4.times { CourseSemester.make(:course => @course, :semester => 1.year.ago.year_semester) }
+    end
+
+    it { @course.current_course_semester.should eql(@course_semester) }
+  end
 end
