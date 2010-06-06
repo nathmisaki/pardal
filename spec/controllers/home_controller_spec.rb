c9fe1c951f0abca533e1a1a3cfdcf926bd8e6f63
Nelson Haraguchi <nelsonmhjr@gmail.com>
2010-06-06 17:08:03 -0300

    Home Controller spec

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,4 +1,10 @@
 require 'spec_helper'
 
 describe HomeController do
+  describe "GET #index" do
+    it "should redirect to /me" do
+      get :index
+      response.should redirect_to('/me')
+    end
+  end
 end
